18a0372f6af555f51e2814113b55dbe2b1973447
AnthonyF525 <Anthony.falice@yahoo.com>
2025-08-19 17:21:28 -0400

    Tractor done

--- src/main/java/com/zipcodewilmington/froilansfarm/model/Tractor.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/model/Tractor.java
@@ -1,5 +1,28 @@
 package com.zipcodewilmington.froilansfarm.model;
 
-public class Tractor {
-    
+import com.zipcodewilmington.froilansfarm.interfaces.Crop;
+import com.zipcodewilmington.froilansfarm.structures.CropRow;
+import com.zipcodewilmington.froilansfarm.interfaces.Edible;
+
+public class Tractor extends Vehicle {
+
+    public Tractor (String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("The " + name + " is making whirling. ");
+    }
+
+    public Edible harvest(Crop crop) {
+        Edible harvestedItem = crop.yieldProduce();
+        System.out.println(crop + "was harvested. ");
+        return harvestedItem;
+
+    }
+
+    public void prepareRow(CropRow cropRow) {
+        System.out.println("The " + cropRow.getName() + " is ready for sowing!");
+    }
 }
