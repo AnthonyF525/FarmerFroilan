64bdd8604b7b691fbb38964f2b34f2e5cb74eb91
AnthonyF525 <Anthony.falice@yahoo.com>
2025-08-20 14:28:59 -0400

    CORNSTALKgit add .git add .

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/model/CornStalk.java
@@ -0,0 +1,50 @@
+package com.zipcodewilmington.froilansfarm.model;
+
+import com.zipcodewilmington.froilansfarm.interfaces.Crop;
+import com.zipcodewilmington.froilansfarm.interfaces.Edible;
+
+public class CornStalk implements Crop {
+    private boolean hasBeenFertilized = false;
+    private boolean hasBeenHarvested = false;
+    
+    @Override
+    public Edible yieldProduce() {
+        if (hasBeenFertilized && !hasBeenHarvested) {
+            hasBeenHarvested = true;
+            return new EarCorn();
+        }
+        return null;
+    }
+    
+    @Override
+    public void fertilize() {
+        hasBeenFertilized = true;
+    }
+    
+    @Override
+    public boolean isHasBeenFertilized() {
+        return hasBeenFertilized;
+    }
+    
+    @Override
+    public void setHasBeenFertilized(boolean hasBeenFertilized) {
+        this.hasBeenFertilized = hasBeenFertilized;
+    }
+    
+    @Override
+    public boolean isHasBeenHarvested() {
+        return hasBeenHarvested;
+    }
+
+    @Override
+    public String getName() {
+        return "Corn Stalk";
+    }
+    
+    @Override
+    public void setHasBeenHarvested(boolean hasBeenHarvested) {
+        this.hasBeenHarvested = hasBeenHarvested;
+    }
+
+
+}
